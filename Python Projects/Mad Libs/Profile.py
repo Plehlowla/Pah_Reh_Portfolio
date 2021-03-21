1a1f8507f202b92b8fcb912d24054a9b25efc04e
Plehlowla <45471747+Plehlowla@users.noreply.github.com>
2021-03-21 00:32:07 -0500

    Add files via upload

---
+++ Python Projects/Mad Libs/Profile.py
@@ -0,0 +1,11 @@
+name = input('Enter your name: ')
+age = input('Enter your age: ')
+school = input('Enter your school: ')
+career = input('What career are you persuding?: ')
+
+
+
+print(f'Profile: {name}')
+print(f'{name} is {age} years old')
+print(f'{name} went to {school}')
+print(f'{name} is persuding a career in {career}')
