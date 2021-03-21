4ee9c963d8e2bc8d3169921b594993bbc2492309
Plehlowla <45471747+Plehlowla@users.noreply.github.com>
2021-03-21 01:15:59 -0500

    Add files via upload

---
+++ Python Projects/Number_Guessing/Number_Guessing_Easy.py
@@ -0,0 +1,22 @@
+import random
+
+print("Let's Play!\nDifficulty is 1 to 10!")
+
+secret = random.randint(1,10)
+
+guess = None
+count = 1
+
+while guess != secret:
+    guess = input('Please guess a number between 1 to 10: ')
+
+    if guess.isdigit():
+        guess = int(guess)
+
+    if guess == secret:
+        print('Congrats you got it! the number was ' + str(secret))
+    else:
+        print('Please try again!')
+        count += 1
+
+print('It took you', count, 'guesses!')
