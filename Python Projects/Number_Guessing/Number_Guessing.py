91d374262bd19d487cd5b900deef5bcae5bedd7a
Plehlowla <45471747+Plehlowla@users.noreply.github.com>
2021-03-21 01:13:13 -0500

    Add files via upload

---
+++ Python Projects/Number_Guessing/Number_Guessing.py
@@ -0,0 +1,31 @@
+import random
+
+flag = True
+while flag:
+    num = input('Select Difficulty, 1 to ')
+
+    if num.isdigit():
+        print("Let's play!")
+        num = int(num)
+        flag = False
+    else:
+        print('Invalid input! Try Again! *Hint : Pick a number*')
+
+secret = random.randint(1,num)
+
+guess = None
+count = 1
+
+while guess != secret:
+    guess = input('Please guess a number between 1 to ' + str(num) + ': ')
+
+    if guess.isdigit():
+        guess = int(guess)
+
+    if guess == secret:
+        print('Congrats you got it! the number was ' + str(secret))
+    else:
+        print('Please try again!')
+        count += 1
+
+print('It took you', count, 'guesses!')
