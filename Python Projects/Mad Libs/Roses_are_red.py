a4747463c2c10c1e2d1649a6107b9dee4fc942a3
Plehlowla <45471747+Plehlowla@users.noreply.github.com>
2021-03-21 00:20:29 -0500

    Add files via upload

---
+++ Python Projects/Mad Libs/Roses_are_red.py
@@ -0,0 +1,9 @@
+red = input('Roses are: ')
+blue = input('Violets are:  ')
+person_1 = input('Enter a person: ')
+person_2 = input('Enter another person: ')
+
+print(f'Roses are {red}')
+print(f'Violets are {blue}')
+print(f'{person_1} love good jokes,')
+print(f'Hope {person_2} loves them, too')
